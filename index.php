9715bee7846b18039c03284006f1dd4d57f687d8
Lolloc99 <lolloc99@yahoo.it>
2022-06-14 16:05:15 +0200

    Aggiunti prodotti

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php 
+require_once __DIR__ . "/Food.php";
+require_once __DIR__ . "/Game.php";
+require_once __DIR__ . "/Accessory.php";
+
+// $crocchette = new Food("Cibo", "C001", "gatto", "20/12/2023", "7.90");
+// echo $crocchette->printInfo();
+
+// $palla = new Game("Giocattolo", "G001", "Piccola", "Gomma", "13.50");
+// echo $palla->printInfo();
+
+// $collare = new Accessory("Accessorio", "A001", "Gatto", "Pelle", "14.30");
+// echo $collare->printInfo();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +34,7 @@
         I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
         L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
         Il pagamento avviene con la carta di credito, che non deve essere scaduta.
-        
+
         BONUS:
         Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto).
     -->
